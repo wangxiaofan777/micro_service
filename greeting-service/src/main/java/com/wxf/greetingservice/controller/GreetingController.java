305f57ed295c1535e1eba6de7aa218c2f8d60f9d
wangxiaofan777 <390871264@qq.com>
2021-05-18 14:22:42 +0800

    配置调试zuul网关

--- greeting-service/src/main/java/com/wxf/greetingservice/controller/GreetingController.java
+++ greeting-service/src/main/java/com/wxf/greetingservice/controller/GreetingController.java
@@ -4,14 +4,16 @@ import com.wxf.greetingservice.service.GreetingService;
 import com.wxf.greetingservice.service.SayHelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "/greeting")
+@RestController
+@RequestMapping(value = "/greeting")
 public class GreetingController {
 
-  /*  @Autowired
-    private GreetingService greetingService;*/
+    /*  @Autowired
+      private GreetingService greetingService;*/
     @Autowired
     private SayHelloService sayHelloService;
 
--- say-hello-world/src/main/java/com/wxf/sayhelloworld/controller/HelloController.java
+++ say-hello-world/src/main/java/com/wxf/sayhelloworld/controller/HelloController.java
@@ -1,11 +1,13 @@
-package com.wxf.sayhelloworld;
+package com.wxf.sayhelloworld.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "/say/hello")
+@RestController
+@RequestMapping(value = "/say/hello")
 public class HelloController {
 
     @Value("${server.port}")
